0348b5378e4e92ef42c802910659c97831613641
kaytee07 <kofi.taylor07@gmail.com>
2024-03-07 12:13:10 +0000

    isOpen function

---
+++ percolation/Percolation.java
@@ -0,0 +1,65 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+public class Percolation {
+    private static final int TOP = 0;
+    private final boolean[][] opened;
+    private final int size;
+    private final int bottom;
+    private int openSites;
+    private final WeightedQuickUnionUF wqf;
+
+    public Percolation(int n){
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+        size = n;
+        bottom = size * size + 1;
+        wqf = new WeightedQuickUnionUF(size * size + 2);
+        opened = new boolean[size][size];
+        openSites = 0;
+    }
+
+    public void open(int row, int col){
+        checkException(row, col);
+        opened[row -1][col - 1] = true;
+        ++openSites;
+
+        if(row == 1){
+            wqf.union(getQuickFindIndex(row, col), TOP);
+        }
+
+        if (row == size){
+            wqf.union(getQuickFindIndex(row, col), bottom);
+        }
+
+        if(row > 1 && isOpen(row-1, col)){
+            wqf.union(getQuickFindIndex(row, col), getQuickFindIndex(row-1, col));
+        }
+
+        if (row > 1 && isOpen(row + 1, col)) {
+            wqf.union(getQuickFindIndex(row, col), getQuickFindIndex(row+1, col));
+        }
+
+        if (row > 1 && isOpen(row, col - 1)) {
+            wqf.union(getQuickFindIndex(row, col), getQuickFindIndex(row, col - 1));
+        }
+
+        if (row > 1 && isOpen(row, col + 1)) {
+            wqf.union(getQuickFindIndex(row, col), getQuickFindIndex(row, col + 1));
+        }
+    }
+
+    public int getQuickFindIndex(int row, int col){
+        return size * (row - 1) + col;
+    }
+
+    public boolean isOpen(int row, int col){
+        checkException(row, col);
+        return opened[row - 1][col -1];
+    }
+
+    public int numOfOpenSites(){
+        return openSites;
+    }
+
+    public boolean isFull
+}
